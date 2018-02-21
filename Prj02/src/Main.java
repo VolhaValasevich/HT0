850c7570eb580f0ee54f644fbba612d5eac1d5de
VolhaValasevich <36482820+VolhaValasevich@users.noreply.github.com>
2018-02-21 22:31:09 +0300

    update

--- Prj02/src/Main.java
+++ Prj02/src/Main.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main (String[] args) throws IOException {
         ArrayList<String> path = new ArrayList<>();
-        path.add("E:/Music1");
+        //path.add("E:/Music1");
         String output = "output.html";       //default output file name
         String inputExample = "\nInput example: C:/SomePath1/ \"D:/Some Path2\" E:/SomePath3/ C:/SomePath4 c:/results.html";
-        /*if (args.length < 1) {
+        if (args.length < 1) {
             System.out.println("Input one or more directories and the name of the output file (optional), separated with whitespaces. "
                     + "If any of the paths contains whitespaces, surround it with quotation marks. " + inputExample);
             return;
@@ -22,7 +22,7 @@ public class Main {
                 System.out.println("Error in line \"" + v + "\". Please check your input." + inputExample);
                 return;
             }
-        }*/
+        }
         if (path.isEmpty()) {
             System.out.println("No directories have been found in your input." + inputExample);
             return;
